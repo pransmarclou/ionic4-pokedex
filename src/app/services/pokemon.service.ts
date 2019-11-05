aae0fd38d9d5b8f717e618f08530fe187fa11093
pransmarclou <fhranzalimorong@gmail.com>
2019-11-05 15:30:04 +0800

    Updated pokemon.service

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
 export class PokemonService {
+  baseUrl = 'https://pokeapi.co/api/v2';
+  imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getPokemon(offset = 0) {
+    return this.http
+      .get('${this.baseUrl}/pokemon?offset=${offset}&limit=25')
+      .pipe(
+        map(result => {
+          return result['results'];
+        }),
+        map(pokemon => {
+          return pokemon.map((poke, index) => {
+            poke.image = this.getPokeImage(offset + index + 1);
+            poke.pokeIndex = offset + index + 1;
+            return poke;
+          });
+        })
+      );
+  }
+
+  findPokemon(search) {
+    return this.http.get('${this.baseUrl}/pokemon/${search}').pipe(
+      map(pokemon => {
+        pokemon['image'] = this.getPokeImage(pokemon['id']);
+        pokemon['pokeIndex'] = pokemon['id'];
+        return pokemon;
+      })
+    );
+  }
+
+  getPokeImage(index) {
+    return '${this.imageUrl}${index}.png';
+  }
+
+   getPokeDetails(index) {
+    return this.http.get(`${this.baseUrl}/pokemon/${index}`).pipe(
+      map(poke => {
+        let sprites = Object.keys(poke['sprites']);
+        poke['images'] = sprites
+          .map(spriteKey => poke['sprites'][spriteKey])
+          .filter(img => img);
+        return poke;
+      })
+    );
+  }
 }
